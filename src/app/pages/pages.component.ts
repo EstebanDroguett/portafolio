a654297cf370b11845c9aa86945131a4b2284fc4
EstebanDroguett <estebandroguetthuerta@gmail.com>
2020-04-05 17:50:30 -0400

    Configuración del Portafolio

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
+  anio: number = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit() {
